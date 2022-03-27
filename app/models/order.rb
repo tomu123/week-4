1af4eedcdfb7c50af0d8d8f84a4c90e4b388fb2a
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-27 18:52:36 -0500

    Create Order Associations and Enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
+  enum order_status: { pending: 'pending', completed: 'completed', cancelled: 'cancelled' }, _prefix: true, _default: 'pending'
   belongs_to :user
+  has_many :order_lines, dependent: :destroy
+  has_many :products, through: :order_lines
 end
