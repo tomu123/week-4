4d7ad2eb5d60bd3685d9abe122602ae1fb7a2af3
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-28 11:11:55 -0500

    Root to Product Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'pages#index'
+  root 'products#index'
   resources :products do
     member do
       post 'add_to_cart'
