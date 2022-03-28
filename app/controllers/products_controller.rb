d255cc80d80612c10c3445dcefae612a134751ea
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-27 22:54:24 -0500

    Product Index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_cart
+
   def index
     @products = Product.all
   end
@@ -14,4 +16,15 @@ class ProductsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  def add_to_cart; end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'pages#index'
-  resources :products
+  resources :products do
+    member do
+      post 'add_to_cart'
+    end
+  end
 end
