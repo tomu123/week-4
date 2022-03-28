ea15e93c66da212a8f986959ae8fb33d1b9e0b8e
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-28 00:20:02 -0500

    Order History

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,17 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = current_user.orders
+  end
+
+  def create; end
+
+  def new; end
+
+  def edit; end
+
+  def show; end
+
+  def update; end
+
+  def destroy; end
+end
