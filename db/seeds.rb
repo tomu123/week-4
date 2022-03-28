978a1a06f25cb26aed180c8b06219c3fb1f22658
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-27 21:27:19 -0500

    Add Faker Gem and Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Address.full_address,
+               email: Faker::Internet.email, password: '123456')
+end
+
+20.times do
+  Product.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.sentence, price: rand(10..100.00).round(2),
+                  stock: rand(1..25))
+end
+
+products = Product.all
+users = User.all
+10.times do
+  order = Order.create!(date: Faker::Date.in_date_period, user: users.sample, order_status: %w[pending completed cancelled].sample)
+  (1..rand(1..5)).each do
+    product = products.sample
+    product = products.sample until product.stock.positive?
+    OrderLine.create!(order: order, product: product, quantity: rand(1..product.stock))
+  end
+end
