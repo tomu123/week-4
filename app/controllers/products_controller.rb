8b88fb5874b84a2e5aefe96cd8e7cdbb4b926aad
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-27 21:54:05 -0500

    Setup Bootstrap and Fontawesome JS Libraries

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,17 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
-  def create
-  end
+  def create; end
 
-  def new
-  end
+  def new; end
 
-  def edit
-  end
+  def edit; end
 
-  def show
-  end
+  def show; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,19 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// import the bootstrap javascript module
+import "bootstrap"
+
+// import the fontawesome javascript module
+import '@fortawesome/fontawesome-free/js/all'
+
+// import the application.scss (we are using assets stylesheet)
+import "../../assets/stylesheets/application"
+
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
