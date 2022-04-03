8ac70719121d25c08b9095e446f0b3540d95bca4
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-03 16:01:18 -0500

    Refactor Cart:Show and add delete line_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+  def total
+    line_items.to_a.map(&:total).reduce(:+)
+  end
 end
