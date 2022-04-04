5d1f4fb82c3e30b0a22f06be0eb74ea5bca51b80
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-04 02:58:17 -0500

    Comment Validations and Order Show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_order, :belong_to_user!, only: %i[edit destroy update show]
+
   def index
     @orders = current_user.orders
   end
@@ -14,4 +17,14 @@ class OrdersController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def belong_to_user!
+    redirect_to products_path if (@order.user != current_user) && !current_user.admin
+  end
 end
