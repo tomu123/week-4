d7b7aa1254aa36818abee9e66887ebe58aa11885
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-04 07:40:05 -0500

    Add Tag Unique Name Validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :product_tags, dependent: :destroy
   has_many :products, through: :product_tags
+  validates :name, uniqueness: { case_sensitive: false }
 end
