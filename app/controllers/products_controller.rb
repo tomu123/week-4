b8999f965b41f34e8b9d76c93faa5b7c46a0a86b
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-03 23:34:12 -0500

    Add Sort by Name and Popularity to Product Index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,12 @@ class ProductsController < ApplicationController
   before_action :only_admin, except: %i[index show]
 
   def index
-    @products = if params[:search_key].blank?
-                  Product.all
-                else
-                  Product.search_by_name(params[:search_key])
-                end
+    @products = Product.all
+    @products = @products.search_by_name(params[:search_key]) unless params[:search_key].blank?
+    unless params[:order_by_popular].blank?
+      @products = @products.unscope(:order).sort_by_popularity(params[:order_by_popular] == 'true').sort_by_name(false)
+    end
+    @products = @products.unscope(:order).sort_by_name(params[:order_by_name] == 'true') unless params[:order_by_name].blank?
   end
 
   def create
