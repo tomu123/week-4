c8677962a107f00df20bc94320e42a026f37b7db
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-05 20:27:44 -0500

    Add Filter Only Admin to ProductTags Controller

--- app/controllers/product_tags_controller.rb
+++ app/controllers/product_tags_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ProductTagsController < ApplicationController
+  before_action :only_admin
+
   def create
     product_tag = ProductTag.new(tag_id: params[:product_tag][:tag], product_id: params[:product_id])
     if product_tag.save
@@ -16,5 +18,11 @@ class ProductTagsController < ApplicationController
     flash[:notice] = "You have deleted #{product_tag.tag.name} from #{product_tag.product.name} tags!"
     product_tag.destroy
     redirect_to product_path(product_tag.product)
+  end
+
+  private
+
+  def only_admin
+    redirect_to products_path unless user_signed_in? && current_user.admin
   end
 end
