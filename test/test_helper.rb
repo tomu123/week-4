dcce47b2aa95571fc35edf77939449131b8a1d57
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-06 12:29:18 -0500

    Add Devise Test Helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,11 @@ module ActiveSupport
     # fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    # Add FactoryBot methods
     include FactoryBot::Syntax::Methods
     FactoryBot.find_definitions
+
+    # Add Devise test helpers for controller and integration tests
+    include Devise::Test::IntegrationHelpers
   end
 end
