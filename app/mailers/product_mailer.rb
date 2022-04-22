f790ad8a6a4bec6c0eae94b7b44be67264dcdd15
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-22 03:48:59 -0500

    variant for stock notification mail

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -2,7 +2,7 @@ class ProductMailer < ApplicationMailer
   def stock_notification
     @product = params[:product]
     recipient = @product.likes.last.user
-    @image_url = Rails.application.routes.url_helpers.url_for(@product.image)
+    @image_url = Rails.application.routes.url_helpers.url_for(@product.image.variant(resize_to_limit: [220, 220]))
     attachments.inline['product.jpg'] = @product.image.variant(resize_to_limit: [220, 220]).download
     mail(to: 'tomu5642514@gmail.com', subject: "Notification Low Stock - Product: #{@product.name}")
     # ProductMailer.with(product: @product).stock_notification.deliver_now
