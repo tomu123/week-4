c562e2912722417509d1e1c015eb1bf0cf8b31e5
Tomu Komatsu <tomu5642514@gmail.com>
2022-04-28 03:12:49 -0500

    Rework cart controller for view

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,31 +7,11 @@ class CartsController < ApplicationController
   def show; end
 
   def checkout
-    unless @cart.line_items.empty?
-      flash[:alert] = []
-      order_lines = []
-      order = Order.create!(date: Time.current, user: current_user)
-      @cart.line_items.each do |li|
-        order_lines << order.order_lines.build(product: li.product, quantity: li.quantity)
-        flash[:alert].push(*order_lines.last.errors.full_messages) unless order_lines.last.valid?
-      end
-      if flash[:alert].empty?
-        order_lines.each(&:save!)
-        order_lines.each do |ol|
-          user = ol.product.likes.last&.user
-          if !user.nil? && (ol.product.stock <= 3)
-            ProductMailer.with(product: ol.product, recipient: user).stock_notification.deliver_later
-          end
-        end
-        @cart.destroy
-        flash[:notice] = 'Your purchase was successful'
-        redirect_to products_url and return
-      else
-        order.destroy
-        redirect_to cart_path and return
-      end
-    end
-    flash.now[:alert] = 'Your Cart is empty!'
+    Cart::CheckoutService.call(@current_user)
+    flash[:notice] = 'Your purchase was successful'
+    redirect_to products_url
+  rescue StandardError => e
+    flash.now[:alert] = e.message.values.flatten
     render :show
   end
 
